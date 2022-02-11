a0374a37a761f3fa262ab6018429ef799580ddf8
AdityaKumar74 <85862040+AdityaKumar74@users.noreply.github.com>
2022-02-11 12:56:54 +0530

    comments added

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ function errorHandler(error) {
 
 
 function clickHandler() {
-    var inputText = txtInput.value;
+    var inputText = txtInput.value; // taking input
 
+    // calling server for processing
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
     .then(json => {
         var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
+        outputDiv.innerText = translatedText; // output
     })
     .catch(errorHandler)
     
